cb979dc0fd28c4618cc287b4b22789bffa6c08c8
sandro.adsc <sandro.adsc@1312dce0-e170-1b3c-a4d6-a0402316e8e6>
2012-11-14 16:51:45 +0000

    Conexão com o banco de dados - MYSQL

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,10 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      
+      "mysql" % "mysql-connector-java" % "5.1.18",
+      "com.typesafe" %% "play-plugins-mailer" % "2.0.2"
+    
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
@@ -16,3 +19,4 @@ object ApplicationBuild extends Build {
     )
 
 }
+ 
