9bf50dab784b4a86672ea9a022985cada1941f00
denilde.barboza <denilde.barboza@1312dce0-e170-1b3c-a4d6-a0402316e8e6>
2012-11-15 19:53:16 +0000

    implementação da Classe Edital a alguns atributos na classe Administrador.

--- app/models/Administrador.java
+++ app/models/Administrador.java
@@ -1,5 +1,14 @@
 package models;
 
 public class Administrador {
-
+	public String nome;
+	public int idAdministrador;
+	public String login;
+	public String senha;
+	
+	public Administrador(){
+		
+	}
+	
+	
 }
