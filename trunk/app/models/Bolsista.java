916b048f17fa5df5cc70f882f15485bc4eea77e2
sandro.adsc <sandro.adsc@1312dce0-e170-1b3c-a4d6-a0402316e8e6>
2012-11-15 04:45:00 +0000

    implementação do crud gerenciarBolsista.scala.html com add do formulário de cadastro de bolsista.

--- trunk/app/models/Bolsista.java
+++ trunk/app/models/Bolsista.java
@@ -1,5 +1,44 @@
 package models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Bolsista {
+
+	public String nome;
+	public String email;
+	public String matricula;
+	
+	public Bolsista(){
+		
+	}
+	
+	public Bolsista(String nome, String email, String matricula){
+		this.nome = nome;
+		this.email = email;
+		this.matricula = matricula;
+	}
+	
+	public String toString() {
+		return String.format("%s - %s - %s", nome, email, matricula);
+	}
+	
+	
+	private static Set<Bolsista> bolsistas;
+	  
+	  static {
+	    bolsistas = new HashSet<Bolsista>();
+	    bolsistas.add(new Bolsista("Alessandro", "sandro.adsc@gmail.com", "201114040444"));
+	    bolsistas.add(new Bolsista("Aldrexler", "aldrexler@hotmail.com", "101010101010"));
+	    bolsistas.add(new Bolsista("Richardson", "rixardon@hotmail.com", "201143432341"));
+	    bolsistas.add(new Bolsista("Renata", "renata_jx@hotmail.com", "2012303030321"));
+	    bolsistas.add(new Bolsista("Ney", "ney.fla@hotmail.com", "20101204398434"));
+	  }
+	  
+	  public static Set<Bolsista> findAll() {
+		  return new HashSet<Bolsista>(bolsistas); }
+	  
+	  public static void add(Bolsista bolsista) {
+		  bolsistas.add(bolsista); }
 
 }
