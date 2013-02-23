efb6c22b60c71157cad49220b408b0e1e015001e
sandro.adsc <sandro.adsc@1312dce0-e170-1b3c-a4d6-a0402316e8e6>
2013-02-23 13:04:36 +0000

    Organizando o Trunk 8

--- sigepi-ifrn2/app/forms/ProjetoForm.java
+++
@@ -1,9 +0,0 @@
-package forms;
-
-import play.data.validation.Constraints.Required;
-
-public class ProjetoForm {
-
-	@Required(message="O campo deve ser preenchido.")
-	public String resposta;
-}
--- sigepi-ifrn2/public/javascripts/main.js
+++
@@ -1,8 +0,0 @@
-$('.alert .close').hover(
-	function() { $(this).addClass('hover'); },
-	function() { $(this).removeClass('hover'); }
-);
-	
-setInterval(function() {
-	$('.alert').fadeOut('fast', function() { $(this).remove(); });
-}, 15000);
