ee62c97f2387c2650c4e65c30556b9a72064ac15
sandro.adsc <sandro.adsc@1312dce0-e170-1b3c-a4d6-a0402316e8e6>
2013-03-14 01:46:18 +0000

    # WEB - Implementação do Web Service RESTful - visualizar editais.

--- trunk/sigepi-ifrn-web/sigepi-ifrn2/app/ws/Usuario.java
+++ trunk/sigepi-ifrn-web/sigepi-ifrn2/app/ws/Usuario.java
@@ -5,12 +5,16 @@ import java.util.List;
 import org.codehaus.jackson.JsonNode;
 
 import models.Edital;
+import play.api.libs.ws.WS;
+import play.api.libs.ws.WS.WSRequestHolder;
+import play.libs.WS.Response;
 import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Usuario extends Controller {
 
+	static final String url = "www";
 	@BodyParser.Of(BodyParser.Json.class)
 	public static Result autenticacao(String login, String senha) {
 
@@ -28,5 +32,12 @@ public class Usuario extends Controller {
         Edital edital = Edital.find.byId(id);
 		
 		return ok(views.html.Editais.visualizar.render(edital));
+	}/*
+	
+	public static Result get(){
+		WSRequestHolder service = WS.url(url);
+		Promise<WS.Response> promisse = aervice.get();
+		Response response = promisse.getClass();
 	}
+	*/
 }
