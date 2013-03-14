8e09c2315ba86a424ab09f8c0e8a2dc37bebe2a9
sandro.adsc <sandro.adsc@1312dce0-e170-1b3c-a4d6-a0402316e8e6>
2013-03-14 01:46:18 +0000

    # WEB - Implementação do Web Service RESTful - visualizar editais.

--- sigepi-ifrn-web/sigepi-ifrn2/app/ws/Servico.java
+++ sigepi-ifrn-web/sigepi-ifrn2/app/ws/Servico.java
@@ -1,7 +1,83 @@
 package ws;
 
+import java.awt.image.renderable.RenderableImage;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import play.mvc.Controller;
+import models.Edital;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.JsonNodeFactory;
+import org.codehaus.jackson.node.ObjectNode;
+
+import controllers.Editais;
+
+//import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
+import play.libs.F.Promise;
+import play.libs.Json;
+import play.libs.WS;
+import play.libs.WS.Response;
+import play.libs.WS.WSRequestHolder;
+import play.mvc.Controller;
+import play.mvc.Result;
+
 
 public class Servico extends Controller{
 
+	//https://api.github.com/events
+	final private static String url = "";
+
+	public static Result getXmlResponse() {
+		return badRequest("Not yet implemented");
+	}
+	
+	public static Result getJsonResponse() {
+		WSRequestHolder service = WS.url(url);
+		Promise<Response> promise = service.get();
+		Response response = promise.get();
+		JsonNode json = response.asJson();
+		
+		ObjectNode info = Json.newObject();
+		info.put("url", url);
+//		info.
+		info.put("size", json.size());
+
+		ObjectNode result = Json.newObject();
+		result.put("message", "OK");
+		result.put("info", info);
+		
+	//	result.putPOJO("teste", new Pessoa());
+		return ok(result);
+	}
+	public static Result getListaEditais(){
+      List<Edital> editais = Edital.find.findList();
+     
+      ArrayList<String> lista = new ArrayList<String>();
+      //  WSRequestHolder service = WS.url(url);
+	//	Promise<Response> promise = service.get();
+	//	Response response = promise.get();
+	//	JsonNode json = response.asJson();
+		
+		ObjectNode info = Json.newObject();
+		//info.put("url", url);
+		//info.put("size", json.size());
+
+		ObjectNode result = Json.newObject();
+		//result.put("message", "OK");
+		//result.put("info", info);
+		
+		for (Edital e : editais) {
+			//result.put("edital",e.getTitulo());
+			lista.add(Json.toJson(e.getTitulo()).toString());
+		}
+		
+		result.putPOJO("editais", lista);
+		
+		//("editais", editais.llistIterator().next().titulo);
+		//result.POJONode(editais.get(1).titulo);
+		//	result.putPOJO("editais:", Edital.find.findList().get();
+		return ok(result);
+	}
 }
