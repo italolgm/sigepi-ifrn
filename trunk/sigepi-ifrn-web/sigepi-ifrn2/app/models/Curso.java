62f440261181337bc177ea815b0832fcc1adc0c8
sandro.adsc <sandro.adsc@1312dce0-e170-1b3c-a4d6-a0402316e8e6>
2013-03-24 00:25:13 +0000

    # Web - Crud Curso ~final~

--- trunk/sigepi-ifrn-web/sigepi-ifrn2/app/models/Curso.java
+++ trunk/sigepi-ifrn-web/sigepi-ifrn2/app/models/Curso.java
@@ -1,7 +1,43 @@
 package models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
 
-public class Curso extends Model{
+/**
+ * Classe responsável pelos cursos do IFRN no banco de dados.
+ * @author Alessandro
+ *
+ */
 
+@Entity
+public class Curso extends Model{
+	
+	@Id
+	public Long id;
+	
+	@Column(unique=true)
+	@Required(message="O campo deve ser preenchido.")
+	public String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public static Finder<Long, Curso> find = new Finder<Long, Curso>(Long.class, Curso.class);
 }
