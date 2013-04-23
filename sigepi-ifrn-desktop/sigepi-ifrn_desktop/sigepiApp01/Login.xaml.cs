51bbaa5df0fc87d62e67989afdff0dc27b1d0eb5
sandro.adsc <sandro.adsc@1312dce0-e170-1b3c-a4d6-a0402316e8e6>
2013-04-23 22:59:21 +0000

    # Desktop - Alterações no Dao.

--- sigepi-ifrn-desktop/sigepi-ifrn_desktop/sigepiApp01/Login.xaml.cs
+++ sigepi-ifrn-desktop/sigepi-ifrn_desktop/sigepiApp01/Login.xaml.cs
@@ -26,64 +26,17 @@ namespace sigepiApp01
         }
 
         private bool Logado = false;
-
-
-        private bool VerificaLogin()
-        {
-            bool result = false;
-            string StringDeConexao = "server=localhost;uid=root;pwd=root;database=sigepi2";
-
-            using (MySqlConnection conexaoMySQL = MySQLDao.getInstancia().getConexao())
-            {
-                conexaoMySQL.ConnectionString = StringDeConexao;
-
-                try
-                {
-                    conexaoMySQL.Open();
-
-                    string mSQL = "select * from usuario where login = '" + textBox1.Text + "' and nome = '" + passwordBox1.Password +"' and is_administrador = 1 " + "  ;";
-
-                    //MessageBox.Show("sql" + mSQL);
-                    MySqlCommand cmd = new MySqlCommand(mSQL, conexaoMySQL);
-
-                    //  MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-
-                    //  DataTable dtMensagens = new DataTable();
-                    //  da.Fill(dtMensagens);
-                    //this.dataGridView1.DataSource = dtMensagens;
-
-
-                    MySqlDataReader dados = cmd.ExecuteReader();
-                    //  SqlDataReader dados = cmd.ExecuteReader();
-                    result = dados.Read();
-
-                }
-                catch (MySqlException msqle)
-                {
-                    MessageBox.Show("Erro de acesso ao MySQL : " + msqle.Message, "Erro");
-                }
-                finally
-                {
-                    conexaoMySQL.Close();
-                }
-
-                return result;
-            }
-        }
-        
-
-        
        
         private void autenticarLogin_Click(object sender, RoutedEventArgs e)
         {
-            bool result = VerificaLogin();
+            bool result = MySQLDao.VerificaLogin(textBox1.Text, passwordBox1.Password);
 
             Logado = result;
 
             if (result)
             {
 
-                MessageBox.Show("Seja bem vindo!");
+                MessageBox.Show("Seja bem-vindo!");
                 FormAdministrador f = new FormAdministrador();
                 f.Show();
 
