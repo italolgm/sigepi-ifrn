252b05a34c88f7965a0193fee0dd57ae28f38b99
sandro.adsc <sandro.adsc@1312dce0-e170-1b3c-a4d6-a0402316e8e6>
2013-04-23 22:59:21 +0000

    # Desktop - Alterações no Dao.

--- trunk/sigepi-ifrn-desktop/sigepi-ifrn_desktop/sigepiApp01/MySQLDao.cs
+++ trunk/sigepi-ifrn-desktop/sigepi-ifrn_desktop/sigepiApp01/MySQLDao.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using MySql.Data.MySqlClient;
 using System.Configuration;
+using System.Windows.Forms;
 
 namespace sigepiApp01
 {
@@ -22,6 +23,34 @@ namespace sigepiApp01
         {
             string conn = ConfigurationManager.ConnectionStrings["sigepiApp01.Properties.Settings.sigepi2ConnectionString"].ToString();
             return new MySqlConnection(conn);
+        }
+
+        public static bool VerificaLogin(String login, String senha)
+        {
+            bool result = false;
+            string StringDeConexao = "server=localhost;uid=root;pwd=root;database=sigepi2";
+
+            using (MySqlConnection conexaoMySQL = MySQLDao.getInstancia().getConexao())
+            {
+                conexaoMySQL.ConnectionString = StringDeConexao;
+                try
+                {
+                    conexaoMySQL.Open();
+                    string mSQL = "select * from usuario where login = '" + login + "' and nome = '" + senha + "' and is_administrador = 1 " + "  ;";
+                    MySqlCommand cmd = new MySqlCommand(mSQL, conexaoMySQL);
+                    MySqlDataReader dados = cmd.ExecuteReader();
+                    result = dados.Read();
+                }
+                catch (MySqlException msqle)
+                {
+                    MessageBox.Show("Erro de acesso ao MySQL : " + msqle.Message, "Erro");
+                }
+                finally
+                {
+                    conexaoMySQL.Close();
+                }
+                return result;
+            }
         }
     }
 }
