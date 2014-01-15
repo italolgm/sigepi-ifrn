c4d3158b2b95780fe96173b88bc63ddf5c96d858
sandro.adsc <sandro.adsc@1312dce0-e170-1b3c-a4d6-a0402316e8e6>
2014-01-15 00:34:42 +0000

    Adicionado os plugins para realização do deploy no servidor.

--- trunk/sigepi-ifrn-web/sigepi-ifrn2/project/Build.scala
+++ trunk/sigepi-ifrn-web/sigepi-ifrn2/project/Build.scala
@@ -1,20 +1,29 @@
 import sbt._
 import Keys._
 import PlayProject._
+import com.github.play2war.plugin._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "SIGEPI - Sistema de Gestao de Pesquisa e Inovacao"
+    val appName         = "SIGEPI"
     val appVersion      = "1.0.0-SNAPSHOT"
 
     val appDependencies = Seq(
       // Add your project dependencies here,
       "mysql" % "mysql-connector-java" % "5.1.18",
-      "com.typesafe" %% "play-plugins-mailer" % "2.0.2"
+      "com.typesafe" %% "play-plugins-mailer" % "2.0.2",
+	  "com.github.play2war.ext" %% "jboss7-reflections-vfs-integration-play2" % "1.0.0",
+	  "com.github.play2war.ext" %% "redirect-playlogger" % "1.0.1"
+	  
+	  //
+	  
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA)
+    	.settings(Play2WarPlugin.play2WarSettings: _*)
+    	.settings(
+      // Add your own project settings here    
+        Play2WarKeys.servletVersion := "3.0"
     )
 
 }
